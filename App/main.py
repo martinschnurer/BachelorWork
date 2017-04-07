a6e2fadf6fa162912fc1fa55122a9304fff85396
martinschnurer <martin.schnurer@gmail.com>
2017-04-07 15:57:40 +0200

    Add optimizer

--- App/main.py
+++ App/main.py
@@ -15,44 +15,46 @@ model = word2vec.Word2Vec.load_word2vec_format(MODEL_CBOW, binary=True)
 charDict = CharDictionary()
 DS = Dataset(model=model)
 
-
-# np.random.random_integers(0,5,[5,3])
-dataset_words_tagger = np.random.random_integers(-5, 5, [5, 5, 80])
-
-sentences = [
-'Martin nebol včera večer doma',
-'Dnes je krásny slnečný deň',
-'Dušan Ravolský vie krásne spievať',
-'Mirinda mi neprišla vôbec vhod',
-'Kikirikíííííí zakotkodákal kohút hneď ráno'
-]
-sentences = [ s.split() for s in sentences ]
-
+# [None, None, charDict.len]
+# None - pocet slov, neviem
+# None - maximalny pocet pismen -> tiez neviem aky bude najvacsi pocet pismen vo vete
+# Ale viem aky bude embedding
 char_inp = tf.placeholder(shape=[None, None, charDict.len], dtype=tf.float32)
 char_multiplication = charTagger.charTaggingGraph(char_inp)
 
 
 word_inp = tf.placeholder(shape=[1, None, 80], dtype=tf.float32)
-# word_data = np.random.random_integers(-5, 5, [9, None, 80])
 word_multiplication = wordGraph.wordPosTagger(word_inp)
 
 
 addition = word_multiplication + char_multiplication
 softmax = tf.nn.softmax(addition)
-
 shapeOfResult = tf.shape(softmax)
 
+prediction = tf.nn.softmax(multiplication)
+cross_entropy = -tf.reduce_sum(target * tf.log(prediction))
+
+optimizer = tf.train.AdamOptimizer(0.01)
+minimize = optimizer.minimize(cross_entropy)
+
+mistakes = tf.not_equal(tf.argmax(target, 1), tf.argmax(prediction, 1))
+error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+
+init = tf.global_variables_initializer()
+
+
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    sess.run(init)
 
     _charInput = DS.getCharGraphInput()
     _wordInput = DS.getWordGraphInput()
-
+    _target = DS.getTarget()
 
     vysledok, sejp = sess.run([softmax, shapeOfResult],
             {
                 word_inp: _wordInput,
-                char_inp: _charInput
+                char_inp: _charInput,
+                target: _target
             }
         )
 
